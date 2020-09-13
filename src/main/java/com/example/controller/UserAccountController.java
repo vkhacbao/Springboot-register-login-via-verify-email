7383bdbe3287befe837780613ca8c3e5186b6c70
vkhacbao <64761625+vkhacbao@users.noreply.github.com>
2020-09-13 11:43:11 +0700

    Update UserAccountController.java

--- src/main/java/com/example/controller/UserAccountController.java
+++ src/main/java/com/example/controller/UserAccountController.java
@@ -68,7 +68,7 @@ public class UserAccountController {
 			mailMessage.setSubject("Complete Registration!");
 			mailMessage.setFrom("nairobley@gmail.com");
 			mailMessage.setText("To confirm your account, please click here : "
-			+"http://localhost:8082/confirm-account?token="+confirmationToken.getConfirmationToken());
+			+"http://localhost:8119/confirm-account?token="+confirmationToken.getConfirmationToken());
 			
 			emailSenderService.sendEmail(mailMessage);
 			
@@ -146,103 +146,6 @@ public class UserAccountController {
 		return modelAndView;
 	}
 	
-	/**
-	 * Display the forgot password page and form
-	 */
-	@RequestMapping(value="/forgot-password", method=RequestMethod.GET)
-	public ModelAndView displayResetPassword(ModelAndView modelAndView, User user) {
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("forgotPassword");
-		return modelAndView;
-	}
-
-	/**
-	 * Receive email of the user, create token and send it via email to the user
-	 */
-	@RequestMapping(value="/forgot-password", method=RequestMethod.POST)
-	public ModelAndView forgotUserPassword(ModelAndView modelAndView, User user) {
-		User existingUser = userRepository.findByEmailIdIgnoreCase(user.getEmailId());
-		if(existingUser != null) {
-			// create token
-			ConfirmationToken confirmationToken = new ConfirmationToken(existingUser);
-			
-			// save it
-			confirmationTokenRepository.save(confirmationToken);
-			
-			// create the email
-			SimpleMailMessage mailMessage = new SimpleMailMessage();
-			mailMessage.setTo(existingUser.getEmailId());
-			mailMessage.setSubject("Complete Password Reset!");
-			mailMessage.setFrom("nairobley@gmail.com");
-			mailMessage.setText("To complete the password reset process, please click here: "
-			+"http://localhost:8082/confirm-reset?token="+confirmationToken.getConfirmationToken());
-			
-			emailSenderService.sendEmail(mailMessage);
-
-			modelAndView.addObject("message", "Request to reset password received. Check your inbox for the reset link.");
-			modelAndView.setViewName("successForgotPassword");
-
-		} else {	
-			modelAndView.addObject("message", "This email does not exist!");
-			modelAndView.setViewName("error");
-		}
-		
-		return modelAndView;
-	}
-
-
-	@RequestMapping(value="/confirm-reset", method= {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView validateResetToken(ModelAndView modelAndView, @RequestParam("token")String confirmationToken)
-	{
-		ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
-		
-		if(token != null) {
-			User user = userRepository.findByEmailIdIgnoreCase(token.getUser().getEmailId());
-			user.setIsEnabled(1);
-		
-			userRepository.save(user);
-			modelAndView.addObject("user", user);
-			modelAndView.addObject("emailId", user.getEmailId());
-			modelAndView.setViewName("resetPassword");
-		} else {
-			modelAndView.addObject("message", "The link is invalid or broken!");
-			modelAndView.setViewName("error");
-		}
-		
-		return modelAndView;
-	}	
-
-	/**
-	 * Receive the token from the link sent via email and display form to reset password
-	 */
-	@RequestMapping(value = "/reset-password", method = RequestMethod.POST)
-	public ModelAndView resetUserPassword(ModelAndView modelAndView, User user) {
-		// ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
-		
-		if(user.getEmailId() != null) {
-			// use email to find user
-			User tokenUser = userRepository.findByEmailIdIgnoreCase(user.getEmailId());
-			tokenUser.setIsEnabled(1);
-			tokenUser.setPassword(encoder.encode(user.getPassword()));
-			// System.out.println(tokenUser.getPassword());
-			userRepository.save(tokenUser);
-			userRepository.save(user);
-			modelAndView.addObject("message", "Password successfully reset. You can now log in with the new credentials.");
-			modelAndView.setViewName("successResetPassword");
-		} else {
-			modelAndView.addObject("message","The link is invalid or broken!");
-			modelAndView.setViewName("error");
-		}
 	
-		return modelAndView;
-	}
 
-@GetMapping("/test")
-public ModelAndView test (ModelAndView modelAndView, User user) {
-	User enabled = userRepository.getIsEnabled(user.getIsEnabled());
-	if (enabled.getIsEnabled()==0) {
-		modelAndView.setViewName("ok");
-	}
-	return modelAndView;
-}
 }
